c767710e10c934c239c498a68f4ed99fe3ff9272
razzytirta <azzi.fachrurazzi@gmail.com>
2023-12-25 05:08:56 +0800

    Widget Dasar

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red[900],
+          title: Text("My App"),
+          centerTitle: true,
+        ),
+        // body: Center(
+        //   child: Container(
+        //     width: 200,
+        //     height: 200,
+        //     color: Colors.amber,
+        //     child: Center(
+        //       child: Text("Hello World"),
+        //     ),
+        //   ),
+        // ),
+
+        // body: Center(
+        //   child: FlutterLogo(
+        //     size: 300,
+        //   ),
+        // ),
+
+        // body: Center(
+        //   child: ElevatedButton(
+        //       onPressed: () {
+        //         print("Click Me");
+        //       },
+        //       child: Text("Click Me")),
+        // ),
+
+        // body: Center(
+        //   child: Icon(
+        //     Icons.home,
+        //     size: 200,
+        //     color: Colors.red[900],
+        //   ),
+        // ),
+
+        // body: Center(
+        //   child: Image(
+        //     image: AssetImage("assets/gambar-1.jpg"),
+        //   ),
+        // ),
+
+        // body: Center(
+        //   child: Image(
+        //     image: NetworkImage("https://picsum.photos/id/237/200/300"),
+        //   ),
+        // ),
+
         body: Center(
-          child: Text("Hello World"),
+          child: Image.network("https://picsum.photos/200/300"),
         ),
       ),
     );
